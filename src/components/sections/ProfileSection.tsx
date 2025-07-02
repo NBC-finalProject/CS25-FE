34b422f2ff40704be68ae3ccac9699c445d16220
wannabeing <wannabeing@naver.com>
2025-07-02 12:07:25 +0900

    chore: 틀린문제 없을 때 디자인 수정

--- src/components/sections/ProfileSection.tsx
+++ src/components/sections/ProfileSection.tsx
@@ -421,7 +421,7 @@ const ProfileSection: React.FC = () => {
                       )}
                     </div>
                   ) : (
-                    <div className="text-center sm:text-left py-12">
+                    <div className="flex flex-col items-center justify-center text-center sm:text-left py-12">
                       <div className="w-16 h-16 bg-gradient-to-r from-green-400 to-emerald-400 rounded-full flex items-center justify-center mx-auto sm:mx-0 mb-4">
                         <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 20 20">
                           <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
